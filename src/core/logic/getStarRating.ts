304c6d314a9b5ca76b089f84a976bc9e4e7a8f02
Caleb Nwaizu <caleb.nwaizu@gotocourse.co>
2024-06-11 15:09:01 +0100

    Add slider

---
+++ src/core/logic/getStarRating.ts
@@ -0,0 +1,16 @@
+export const getStarRating = (rating: number) => {
+  rating = Math.max(0.5, Math.min(5, rating));
+  const initial_stars = new Array(5).fill("☆");
+
+  const full_stars = Math.floor(rating);
+  const has_half_star = rating % 1 !== 0;
+  if (rating > 0.5) {
+    for (let i = 0; i < full_stars; i += 1) {
+      initial_stars[i] = "★";
+    }
+  }
+  if (has_half_star) {
+    initial_stars[full_stars] = "⯨";
+  }
+  return initial_stars;
+};
