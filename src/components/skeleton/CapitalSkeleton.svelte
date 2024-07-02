7e5520b2caa0cf767c8bf495e590b36c8a36e006
Caleb Nwaizu <caleb.nwaizu@gotocourse.co>
2024-07-03 00:05:20 +0100

    Adjust skeleton

--- src/components/skeleton/CapitalSkeleton.svelte
+++ src/components/skeleton/CapitalSkeleton.svelte
@@ -3,11 +3,16 @@
   import "./style.css";
 </script>
 
-<section class="grid grid-cols-2 gap-2 overflow-y-scroll h-[60vh] ">
-  {#each thirty_six_content as _}
-    <div
-      style=""
-      class="p-4 cursor-pointer pulse h-20 flex items-center justify-center"
-    ></div>
-  {/each}
+<section class="xl:grid xl:grid-cols-2 xl:pt-28 xl:gap-4 xl:w-[85vw] xl:mx-auto h-[85vh]">
+  <div class="grid grid-cols-2 gap-2 overflow-y-scroll w-full">
+    {#each thirty_six_content as _}
+      <div
+        style=""
+        class="p-4 cursor-pointer pulse h-20 flex items-center justify-center"
+      ></div>
+    {/each}
+  </div>
+  <div class="w-full right_pulse bg-gray-200 h-[80vh]">
+
+  </div>
 </section>
