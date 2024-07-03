9046230cca36d396af82359b624c50a0d45278c2
Caleb Nwaizu <caleb.nwaizu@gotocourse.co>
2024-07-04 00:31:19 +0100

    remove baw

--- src/App.svelte
+++ src/App.svelte
@@ -10,6 +10,8 @@
   import Reviews from "./pages/home/capital/[capital]/[organization]/reviews/Index.svelte";
   import CreateOrganization from "./pages/home/organization/create/Index.svelte";
   import AdminDashboard from "@src/pages/admin/dashboard/index.svelte";
+  import AdminOrganization from "@src/pages/admin/dashboard/organization/index.svelte";
+  import AdminUsers from "@src/pages/admin/dashboard/user/index.svelte";
   import { onMount } from "svelte";
   import { store } from "./lib/store";
   import { useToken, useUserData } from "./core/utils/utils";
@@ -76,8 +78,12 @@
     <Route path="/home/capital/:capital/:company/review" component={Review} />
     <Route path="/home/capital/:capital/:company/reviews" component={Reviews} />
     <Route path="/home/organization/add" component={CreateOrganization} />
+  </Router>
 
-    <!-- Admin Routes -->
-    <Route path="/admin/dashboard" component={AdminDashboard} />
+  <!-- Admin Routes -->
+  <Router basepath="admin">
+    <Route path="/dashboard" component={AdminDashboard} />
+    <Route path="/dashboard/organization" component={AdminOrganization} />
+    <Route path="/dashboard/user" component={AdminUsers} />
   </Router>
 </main>
