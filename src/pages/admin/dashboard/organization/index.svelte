22e3bc0548fd2f39aac03105e79047c5a85ff931
Caleb Nwaizu <caleb.nwaizu@gotocourse.co>
2024-07-09 08:14:45 +0100

    Fix button

--- src/pages/admin/dashboard/organization/index.svelte
+++ src/pages/admin/dashboard/organization/index.svelte
@@ -1,10 +1,26 @@
-<script>
+<script lang="ts">
+  import Navbar from "@src/components/admin/navbar/index.svelte";
+  import QuickInfoWidget from "@src/components/admin/quickInfoWidget.svelte";
   import Sidebar from "@src/components/admin/sidebar.svelte";
+  import Table from "@src/components/admin/table.svelte";
 
-
+  const user_columns = [{ title: "Name", label: "name" }];
+  const organizations: any[] = [];
 </script>
 
+<main class="bg-gray-900 h-screen flex items-start text-white gap-10">
+  <Navbar />
+  <Sidebar />
 
-<main class="bg-gray-900">
-    <Sidebar />
+  <div class="flex flex-col w-full p-10 text-white">
+    <div class="flex flex-col w-full p-10">
+      <div class="py-14 pt-28 flex items-start gap-10">
+        <QuickInfoWidget
+          title="Total Organizations"
+          value={organizations.length}
+        />
+      </div>
+      <Table columns={user_columns} rows={organizations} />
+    </div>
+  </div>
 </main>
