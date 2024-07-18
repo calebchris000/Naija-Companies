ed5d9dae741bdfe282a7164ac001ba01ea297a93
Caleb Nwaizu <caleb.nwaizu@gotocourse.co>
2024-07-18 10:42:52 +0100

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: "#b4c5e4",
+        secondary: "#3c3744",
+        cto: "#090c9b",
+        skyblue: "#3d52d5",
+        light: "#fbfff1",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
