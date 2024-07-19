fbdd524c34d91659b5ef03e8eeffd3a40ab527d0
Caleb Nwaizu <caleb.nwaizu@gotocourse.co>
2024-07-19 13:46:26 +0100

    Move best-rated out of components

--- src/components/Landing/best-rated.svelte
+++ src/components/Landing/best-rated.svelte
@@ -1,11 +1,13 @@
 <script>
-  import CompanyCard from "./company-card.svelte";
-  import CompanyReview from "./company-review.svelte";
+  import CompanyCard from "./components/company-card.svelte";
+  import CompanyReview from "./components/company-review.svelte";
 </script>
 
 <section class="xl:p-32 p-4 h-full py-20 space-y-10 xl:h-screen">
   <div class="flex flex-col gap-4 items-center font-medium text-secondary">
-    <span class="text-center text-2xl xl:text-3xl">Best Rated Organization Of 2024</span>
+    <span class="text-center text-2xl xl:text-3xl"
+      >Best Rated Organization Of 2024</span
+    >
     <span class="xl:max-w-[50vw] text-center font-normal leading-loose"
       >Discover top-rated Nigerian employers across industries.
       Employee-verified excellence based on workplace satisfaction and company
