962dec8f25e59ee9a8c66e54befbb7cf411ef9fc
Caleb Nwaizu <calebchris000@gmail.com>
2024-07-22 10:50:09 +0100

    Desktop about-us done

--- src/pages/home/Landing.svelte
+++ src/pages/home/Landing.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-  import AboutUs from "@src/components/Landing/about-us.svelte";
-import BestRated from "@src/components/Landing/best-rated.svelte";
-  import Hero from "@src/components/Landing/hero.svelte";
-  import Sector from "@src/components/Landing/sector.svelte";
-  import Stats from "@src/components/Landing/stats.svelte";
-  import Navbar from "@src/components/navbar/navbar.svelte";
+    import AboutUs from "@src/components/Landing/about-us.svelte";
+    import BestRated from "@src/components/Landing/best-rated.svelte";
+    import Hero from "@src/components/Landing/Hero.svelte";
+    import Sector from "@src/components/Landing/sector.svelte";
+    import Stats from "@src/components/Landing/stats.svelte";
+    import Navbar from "@src/components/navbar/navbar.svelte";
 </script>
 
 <section class="">
-  <Navbar />
-  <Hero />
-  <Stats />
-  <Sector />
-  <BestRated />
-  <AboutUs />
+    <Navbar />
+    <Hero />
+    <Stats />
+    <Sector />
+    <BestRated />
+    <AboutUs />
 </section>
 
 <svelte:head>
-  <title>Naija Companies - Home</title>
+    <title>Naija Companies - Home</title>
 </svelte:head>
 
 <style>
