f360db2997ce45bd06c74161e4c34f3b0284b2e6
Caleb Nwaizu <calebchris000@gmail.com>
2024-07-23 22:54:25 +0100

    Complete landing page

--- src/components/Landing/footer.svelte
+++ src/components/Landing/footer.svelte
@@ -1,10 +1,10 @@
 <script>
 </script>
 
-<section class="h-screen w-full bg-skyblue p-32 text-primary">
+<section class=" w-full bg-skyblue p-32 pb-4 px-16 text-primary space-y-24">
     <div class="flex flex-col gap-2 text-center">
         <span class="text-3xl font-medium">Join Our Newsletter</span>
-        <span class="leading-loose max-w-[60vw] mx-auto"
+        <span class="leading-loose max-w-[55vw] mx-auto text-sm"
             >Stay informed about Nigerian workplace trends, company reviews, and
             career insights. Get exclusive updates and be the first to know
             about newly reviewed organizations. Join our community today!</span
@@ -19,6 +19,60 @@
                 class="bg-cto h-full px-10 rounded-md font-semibold"
                 type="button">Submit</button
             >
+        </div>
+    </div>
+
+    <div class="flex flex-col gap-10">
+        <span>Logo</span>
+
+        <div class="flex gap-60">
+            <figure class="flex flex-col gap-4">
+                <span class="font-semibold">Follow Us</span>
+                <div class="flex flex-col gap-4 text-sm">
+                    <span>Facebook</span>
+                    <span>Twitter</span>
+                    <span>LinkedIn</span>
+                    <span>Instagram</span>
+                </div>
+            </figure>
+            <figure class="flex flex-col gap-4">
+                <span class="font-semibold">About</span>
+                <div class="flex flex-col gap-4 text-sm">
+                    <span>About Us</span>
+                    <span>Blog</span>
+                    <span>Services</span>
+                    <span>Features</span>
+                </div>
+            </figure>
+            <figure class="flex flex-col gap-4">
+                <span class="font-semibold">Community</span>
+                <div class="flex flex-col gap-4 text-sm">
+                    <span>Help Center</span>
+                    <span>Login</span>
+                    <span>Signup</span>
+                    <span>Home</span>
+                </div>
+            </figure>
+            <figure class="flex flex-col gap-4">
+                <span class="font-semibold">Community</span>
+                <div class="flex flex-col gap-4 text-sm">
+                    <span>Help Center</span>
+                    <span>Login</span>
+                    <span>Signup</span>
+                    <span>Home</span>
+                </div>
+            </figure>
+        </div>
+    </div>
+
+    <div class="text-primary font-medium text-xs flex items-center gap-4">
+        <span>© 2024 Naija Companies. All rights reserved. </span>
+        <span>|</span>
+        <div class="flex gap-4">
+            <span>Legal</span>
+            <span>Cookies</span>
+            <span>Privacy Policy</span>
+            <span>Terms & Conditions</span>
         </div>
     </div>
 </section>
