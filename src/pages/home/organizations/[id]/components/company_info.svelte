99d5cfc4ae33a0d65573ac7e905cf47b2b689538
Caleb Nwaizu <calebchris000@gmail.com>
2024-08-13 17:54:13 +0100

    Setup id page

---
+++ src/pages/home/organizations/[id]/components/company_info.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<div class="bg-primary w-full rounded-3xl h-[50vh]"></div>
--- src/pages/home/organizations/[id]/index.svelte
+++ src/pages/home/organizations/[id]/index.svelte
@@ -1,6 +1,36 @@
 <script lang="ts">
+    import Pencil from "@src/assets/svg/Pencil.svelte";
+    import Navbar from "../components/navbar.svelte";
+    import CompanyInfo from "./components/company_info.svelte";
+    import { getStarRating } from "@src/core/logic/getStarRating";
+
     const params_id = window.location.href.split("/").slice(-1).join("");
     console.log(params_id);
+
+    const stars = getStarRating(5).join("");
 </script>
 
-<section class="h-screen 2xl:h-screen bg-secondary">id is {params_id}</section>
+<section class="h-screen 2xl:h-screen bg-secondary">
+    <Navbar />
+    <section class="mx-auto mt-10 px-56">
+        <div class="flex flex-col gap-4">
+            <CompanyInfo />
+            <div
+                class="bg-primary w-full h-24 rounded-3xl p-4 px-8 flex justify-between items-center"
+            >
+                <button
+                    class="text-secondary flex items-center gap-4"
+                    type="button"
+                >
+                    <span class="">Write a review</span>
+                    <Pencil className="w-4" />
+                </button>
+
+                <button class="text-secondary text-4xl" type="button"
+                    >{stars}</button
+                >
+            </div>
+        </div>
+        <div></div>
+    </section>
+</section>
