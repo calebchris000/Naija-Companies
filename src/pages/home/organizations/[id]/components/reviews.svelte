5d98cb603ed75026b8fd34654ab0dd5de54a3f0d
Caleb Nwaizu <calebchris000@gmail.com>
2024-08-18 16:57:31 +0100

    Make responsive

--- src/pages/home/organizations/[id]/components/reviews.svelte
+++ src/pages/home/organizations/[id]/components/reviews.svelte
@@ -133,10 +133,10 @@
                 <div
                     class="flex relative flex-col items-start xl:items-center xl:flex-row px-10 gap-2 xl:gap-0 py-5 bg-gray-100 justify-between"
                 >
-                    <span class="font-medium text-xl xl:text-lg"
+                    <span class="font-medium text-xl text-nowrap xl:text-lg"
                         >{review.title}</span
                     >
-                    <div class="flex w-full items-center gap-10 relative">
+                    <div class="flex w-full items-center xl:justify-end gap-10">
                         <span
                             style="color: {getStarColor(review.rating)}"
                             class="text-2xl flex gap-1"
@@ -155,7 +155,7 @@
                             <button
                                 on:click={() => {
                                     console.log("clicekd");
-                                    rating_options_open = true;
+                                    rating_options_open = !rating_options_open;
                                 }}
                                 type="button"
                                 class="xl:flex hidden option flex-col items-center gap-[0.1rem]"
@@ -166,29 +166,29 @@
                                     ></span>
                                 {/each}
                             </button>
-                            <div
-                                class:opacity-100={rating_options_open}
-                                class:pointer-events-auto={rating_options_open}
-                                class="absolute option opacity-0 pointer-events-none flex flex-col rounded-lg overflow-hidden transition-all translate-x-10 left-0 right-0 top-16 bg-white shadow-lg"
-                            >
-                                <button
-                                    on:click={() => handleDelete(review.id)}
-                                    type="button"
-                                    class="hover:bg-primary option rating_option justify-between flex p-2 px-4 items-center gap-4 w-full transition-all hover:text-secondary"
-                                >
-                                    <span>Delete</span>
-                                    <Trash className="w-4 text-red-500" />
-                                </button>
-                            </div>
                         {/if}
+                        <div
+                            class:opacity-100={rating_options_open}
+                            class:pointer-events-auto={rating_options_open}
+                            class="absolute option opacity-0 pointer-events-none min-w-44 max-w-72 flex flex-col rounded-lg overflow-hidden transition-all translate-x-10 right-10 top-28 xl:top-16 bg-white shadow-lg"
+                        >
+                            <button
+                                on:click={() => handleDelete(review.id)}
+                                type="button"
+                                class="hover:bg-primary option rating_option justify-between flex p-4 xl:p-2 px-4 items-center gap-4 w-full transition-all hover:text-secondary"
+                            >
+                                <span>Delete</span>
+                                <Trash className="w-4 text-red-500" />
+                            </button>
+                        </div>
                     </div>
                     <button
                         on:click={() => {
                             console.log("clicekd");
-                            rating_options_open = true;
+                            rating_options_open = !rating_options_open;
                         }}
                         type="button"
-                        class="grid place-items-center py-8 hover:bg-gray-400 transition-all absolute right-0 h-full w-12 top-[50%] -translate-y-[50%] option flex-col items-center gap-[0.1rem]"
+                        class="grid option xl:hidden place-items-center py-8 hover:bg-gray-400 transition-all absolute right-0 h-full w-12 top-[50%] -translate-y-[50%] option flex-col items-center gap-[0.1rem]"
                     >
                         {#each Array(3) as _}
                             <span class="w-2 h-2 option rounded-full bg-primary"
