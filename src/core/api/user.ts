74b32a65991cdf860bb4402ce5177460321ced6c
Caleb Nwaizu <calebchris000@gmail.com>
2024-08-19 11:19:15 +0100

    Ensure signup is set

--- src/core/api/user.ts
+++ src/core/api/user.ts
@@ -31,3 +31,35 @@ export const GetUsers = async ({
     }
   }
 };
+
+export const GetUser = async ({
+  token,
+  userId,
+}: {
+  token: string;
+  userId: string;
+}) => {
+  try {
+    const base_url = import.meta.env.VITE_NAIJA_COMPANIES_BASE_URL;
+    const response = await axios.get(`${base_url}/user?userId=${userId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const { status, data } = response;
+    return { status, data };
+  } catch (error: any) {
+    if (error?.response) {
+      return {
+        status: 403,
+        data: error.response?.data.message,
+      };
+    } else {
+      return {
+        status: 403,
+        data: String(error),
+      };
+    }
+  }
+};
